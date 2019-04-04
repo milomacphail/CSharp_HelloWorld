40821c181e5134aeb2c2d08c1d59a4e572861ac1
milomacphail <milomacphail@gmail.com>
2019-04-03 21:22:48 -0400

    Basic data types added

--- variables_and_constants/Program.cs
+++ variables_and_constants/Program.cs
@@ -6,8 +6,23 @@ namespace variables_and_constants
     {
         public static void Main(string[] args)
         {
-            byte number;
+            byte number = 2;
+            int count = 10;
+            float totalPrice =  20.95f;
+            char character = 'A';
+            string firstName = "Milo";
+            bool isWorking = true;
+            const float Pi = 3.14f;
+
             Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
         }
     }
 }
