c226ffa934d3f58bdf39caaa0c211c5bb33cb602
milomacphail <milomacphail@gmail.com>
2019-04-03 20:53:47 -0400

    Variables and Constants practice

---
+++ variables_and_constants/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace variables_and_constants
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
